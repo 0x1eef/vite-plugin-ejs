15c0ea932c6d43e479cfc9ba502db36ad0715dd3
0x1eef <0x1eef@protonmail.com>
2023-04-25 01:05:31 -0300

    Rebuild when input changes (with build --watch)

--- src/index.ts
+++ src/index.ts
@@ -36,19 +36,30 @@ type Vite = {
 export default function(...files: File[]): PluginOption {
   const vite: Vite = {root: "", build: {outDir: ""}};
   const fileOpts = { encoding: 'utf-8' };
+  const watched: Set<string> = new Set(...(function() {
+    const inputs = []
+    for (const k in files) {
+      inputs.push(files[k].input);
+    }
+    return inputs;
+  })());
 
   return {
     name: 'ejs',
     enforce: 'pre',
+    buildStart() {
+      watched.forEach((input) => {
+        this.addWatchFile(path.join(vite.root, input))
+      });
+    },
     async generateBundle(_options, _bundle): Promise<void> {
       try {
         for (const k in Object.keys(files)) {
           const file = files[k]
           const input = path.join(vite.root, file.input)
           const { variables } = file
-          const text = await render(await readFile(input, fileOpts), variables)
-          const output = path.join(vite.root, vite.build.outDir, file.output)
-          await writeFile(output, text, fileOpts);
+          const text = render(await readFile(input, fileOpts), variables)
+          this.emitFile({type: 'asset', fileName: file.output, source: text})
         }
       } catch (error: any) {
         this.error(error)
