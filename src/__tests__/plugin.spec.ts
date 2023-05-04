47b2c587e2a08c05ae76d07d09c8fb6cc9cd969d
0x1eef <0x1eef@protonmail.com>
2023-05-04 05:11:24 -0300

    Return 'Plugin' type
    And apply eslint / prettier configuration

--- src/__tests__/plugin.spec.ts
+++ src/__tests__/plugin.spec.ts
@@ -1,24 +1,23 @@
-import type { Plugin } from "vite";
-import ejs from "../index";
-import { describe, test, expect } from "vitest";
+import ejs from '../index';
+import { describe, test, expect } from 'vitest';
 
 describe('vite-plugin-ejs', () => {
-  const files = [ {input: "", output: "", variables: {} } ]
+  const files = [ { input: '', output: '', variables: {} } ];
 
   test('the plugin name', () => {
-    const plugin = ejs({files})
-    expect(plugin.name).toEqual('ejs')
-  })
+    const plugin = ejs({ files });
+    expect(plugin.name).toEqual('ejs');
+  });
 
   describe('the enforce option', () => {
     test('the default is "pre"', () => {
-      const plugin = ejs({files})
-      expect(plugin.enforce).toEqual('pre')
-    })
+      const plugin = ejs({ files });
+      expect(plugin.enforce).toEqual('pre');
+    });
 
     test('the option can be set to "post"', () => {
-      const plugin = ejs({enforce: 'post', files})
-      expect(plugin.enforce).toEqual('post')
-    })
-  })
-})
+      const plugin = ejs({ enforce: 'post', files });
+      expect(plugin.enforce).toEqual('post');
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,41 @@
-import { PluginOption, ResolvedConfig } from "vite";
+import { Plugin, ResolvedConfig } from 'vite';
 import { render } from 'ejs';
 import path from 'path';
 import fs from 'fs';
 
 type Vite = {
-  root: string, build: {outDir: string}
-}
+  root: string
+  build: {outDir: string}
+};
 
 type File = {
-  input: string,
+  input: string
   output: string
   variables: Record<string, string | Function | object>
-}
+};
 
 type Options = {
-  enforce?: "pre" | "post",
-  apply?: "build" | "serve",
+  enforce?: 'pre' | 'post'
+  apply?: 'build' | 'serve'
   files: File[]
-}
+};
 
 type ReadFile = [string, object];
 const readFile = async (...args: ReadFile): Promise<string> => {
   const [path, options] = args;
-  return new Promise((resolve, reject) => {
+  return await new Promise((resolve, reject) => {
     fs.readFile(path, options, (err, data) => {
       err ? reject(err) : resolve(data.toString());
     });
   });
 };
 
-export default function(options: Options): PluginOption {
-  const { files } = options
-  const vite: Vite = {root: "", build: {outDir: ""}};
+export default function(options: Options): Plugin {
+  const { files } = options;
+  const vite: Vite = { root: '', build: { outDir: '' } };
   const fileOpts = { encoding: 'utf-8' };
   const watched: Set<string> = new Set(...(function() {
-    const inputs: string[] = []
+    const inputs: string[] = [];
     files.forEach((file) => {
       inputs.push(file.input);
     });
@@ -43,27 +44,29 @@ export default function(options: Options): PluginOption {
 
   return {
     name: 'ejs',
-    enforce: options.enforce || "pre",
+    enforce: options.enforce || 'pre',
     apply: options.apply,
     buildStart() {
       watched.forEach((input) => {
-        this.addWatchFile(path.join(vite.root, input))
+        this.addWatchFile(path.join(vite.root, input));
       });
     },
-    async generateBundle(_options, _bundle): Promise<void> {
+    async generateBundle(_options, _bundle) {
       try {
-        files.forEach(async (file) => {
-          const input = path.join(vite.root, file.input)
-          const { variables } = file
-          const text = render(await readFile(input, fileOpts), variables)
-          this.emitFile({type: 'asset', fileName: file.output, source: text})
+        files.forEach((file) => {
+          (async () => {
+            const input = path.join(vite.root, file.input);
+            const { variables } = file;
+            const source = render(await readFile(input, fileOpts), variables);
+            this.emitFile({ type: 'asset', fileName: file.output, source });
+          })();
         });
       } catch (error: any) {
-        this.error(error)
+        this.error(error);
       }
     },
     configResolved(resolvedConfig: ResolvedConfig) {
-      Object.assign(vite, resolvedConfig)
+      Object.assign(vite, resolvedConfig);
     },
-  }
+  };
 }
